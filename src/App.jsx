56122b9a87786a430de237078494e9c07833c985
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-05-26 15:44:10 -0400

    need random and HSL display

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    hue: 0,
+    saturation: 0,
+    lightness: 0,
+  }
+
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <>
+        <h1>Color Picker</h1>
+        <h4>Color Wheel</h4>
+        <div
+          className="ColorRange"
+          style={{ backgroundColor: `hsl${this.state.hue}, 100%, 50%` }}
+        ></div>
+        <p> Hue: {this.state.hue}</p>
+        <input
+          max={359}
+          type="range"
+          value={this.state.hue}
+          onChange={event => this.setState({ hue: event.target.value })}
+        />
+        <p> Saturation: {this.state.saturation}</p>
+        <input
+          max={100}
+          type="range"
+          value={this.state.saturation}
+          onChange={event => this.setState({ saturation: event.target.value })}
+        />
+        <p> Lightness: {this.state.lightness}</p>
+        <input
+          max={100}
+          type="range"
+          value={this.state.lightness}
+          onChange={event => this.setState({ lightness: event.target.value })}
+        />
+      </>
+    )
   }
 }
+
+export default App
